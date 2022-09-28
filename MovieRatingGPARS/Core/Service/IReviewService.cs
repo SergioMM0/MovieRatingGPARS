5022f5ca6d73fb80027761a320b7bcc7e05036f9
Renaldinho <rena0968@easv365.dk>
2022-09-28 09:56:46 +0200

    interface methods

--- MovieRatingGPARS/Core/Service/IReviewService.cs
+++ MovieRatingGPARS/Core/Service/IReviewService.cs
@@ -3,4 +3,23 @@ namespace MovieRatingGPARS.Core.Service;
 public interface IReviewService
 {
     int GetNumberOfReviewsFromReviewer(int reviewer);
-}
+    
+    double GetAverageRateFromReviewer(int reviewer);
+    
+    int GetNumberOfRatesByReviewer(int reviewer, int rate);
+
+    int GetNumberOfReviews(int movie);
+    
+    double GetAverageRateOfMovie(int movie);
+
+    int GetNumberOfRates(int movie, int rate);
+
+    List<int> GetMoviesWithHighestNumberOfTopRates();
+
+    List<int> GetMostProductiveReviewers();
+    
+    List<int> GetTopRatedMovies(int amount);
+    
+    List<int> GetTopMoviesByReviewer(int reviewer);
+    
+    List<int> GetReviewersByMovie(int movie);}
