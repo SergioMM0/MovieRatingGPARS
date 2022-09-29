d1c7139645f8eb01d4ca6a716192ed6c62795c7b
Sergio M <serg0971@easv365.dk>
2022-09-29 09:18:47 +0200

    .

--- MovieRatingGPARS/Aplication/ReviewService.cs
+++ MovieRatingGPARS/Aplication/ReviewService.cs
@@ -222,4 +222,69 @@ public class ReviewService : IReviewService
     {
         throw new NotImplementedException();
     }
+
+    public double GetAverageRateFromReviewer(int reviewer)
+    {
+        var count = 0;
+        var total = 0;
+        foreach (BEReview review in _repository.GetAll())
+        {
+            if (review.Reviewer == reviewer)
+            {
+                count++;
+                total += review.Grade;
+            }
+        }
+
+        if (count!=0)
+            return total/count;
+        
+            return 0;
+        
+    }
+
+    public int GetNumberOfRatesByReviewer(int reviewer, int rate)
+    {
+        throw new NotImplementedException();
+    }
+
+    public int GetNumberOfReviews(int movie)
+    {
+        throw new NotImplementedException();
+    }
+
+    public double GetAverageRateOfMovie(int movie)
+    {
+        throw new NotImplementedException();
+    }
+
+    public int GetNumberOfRates(int movie, int rate)
+    {
+        throw new NotImplementedException();
+    }
+
+    public List<int> GetMoviesWithHighestNumberOfTopRates()
+    {
+        throw new NotImplementedException();
+    }
+
+    public List<int> GetMostProductiveReviewers()
+    {
+        throw new NotImplementedException();
+    }
+
+    public List<int> GetTopRatedMovies(int amount)
+    {
+        throw new NotImplementedException();
+    }
+
+    public List<int> GetTopMoviesByReviewer(int reviewer)
+    {
+        throw new NotImplementedException();
+    }
+
+    public List<int> GetReviewersByMovie(int movie)
+    {
+        throw new NotImplementedException();
+    }
 }
